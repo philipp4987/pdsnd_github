0c24a4603d02c02e4d0aa701dde9d9a27f28e0c3
data570 Camp570 <data.camp570@audi.de>
2023-11-03 11:32:26 +0100

    refactor: change wording for restart option.

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -379,7 +379,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart the analysis? Please Enter yes or no.\n')
         if restart.lower() != 'yes':
             print ("\nThank you for using this service!\nGoodbye!")
             break
